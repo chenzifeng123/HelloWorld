22d474bdae210ad69d574fce8284f962ef13bf3b
chenzifeng123 <2277599150@qq.com>
2019-03-12 21:04:42 +0800

    Initial commit

---
+++ app/src/main/java/edu/fjnu/cse/helloworld/MainActivity.java
@@ -0,0 +1,40 @@
+package edu.fjnu.cse.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    //private static final String
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i("MainActivityLife","调用onCreate（）");
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.i("MainActivityLife","调用onStart（）");
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.i("MainActivityLife","调用onResume（）");
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.i("MainActivityLife","调用onPause（）");
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.i("MainActivityLife","调用onDestroy（）");
+    }
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.i("MainActivityLife","调用onRestart（）");
+    }
+}
